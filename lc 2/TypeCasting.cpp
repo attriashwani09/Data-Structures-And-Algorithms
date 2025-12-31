b3b9a6c34e597fda1a6640dbce1ebfb9ca2521e7
attriashwani09 <attriashwani09@gmail.com>
2025-12-31 21:15:43 +0530

    Data Types and Type Casting

---
+++ lc 2/TypeCasting.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // Type Casting in C++
+
+    int a='a';
+    cout<< a << endl;
+    // Output = 97 
+    // becuase char is stored as ASCII value , and a ASCII value is 97
+
+    char ch=98;
+    cout << ch << endl;   // b
+
+    char ch2= 123456;
+    cout<< ch2 << endl;  // @  (In the last bit of this num (int int ) 64  is present and in ASCII 64 =@)
+
+
+}
--- lc 2/first.cpp
+++ lc 2/first.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello World !!";
+    
+    // cout<<"Hello World !!"<< endl;
+
+    // Data Types in c++
+    int a=10;
+    cout<< a << endl ;
+
+    char ch='d';
+    cout<< ch <<  endl;
+
+    bool bl=false;
+    cout<< bl << endl;
+
+    float b =1.23;
+    cout << b << endl;
+
+    double c=5.562;
+    cout<< c << endl;
+
+
+    // Sizeof() function 
+    int size= sizeof(a);
+    cout<< "Size of a =" << size << endl;
+
+
     return 0;
 }
