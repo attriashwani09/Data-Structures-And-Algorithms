1ed0548c7bd7cb8cd9b289e695e364377cc0640b
attriashwani09 <attriashwani09@gmail.com>
2026-03-11 10:22:46 +0530

    Minor Changes"

--- lec 24 Basic Maths for DSA/GCD (HCF of 2 numbers)/code.cpp
+++ lec 24 Basic Maths for DSA/GCD (HCF of 2 numbers)/code.cpp
@@ -2,7 +2,14 @@
 using namespace std; 
 
 
-int gcd( int a , int b ) {
+int gcd( int a , int b ) { 
+
+
+    if( a== 0 )
+        return b; 
+
+    if ( b== 0)
+        return a;
 
     while( a != b ) {
 
